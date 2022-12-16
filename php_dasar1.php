7cbba788e3aaabac652dccf4dbc962ed6f1c01cc
Muhamad Ilham Rosidi <muhamadilhamrosidi0707@gmail.com>
2022-12-16 17:59:15 +0700

    php_dasar1

---
+++ php_dasar1.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = "Muhamad Ilham Rosidi";
+$b = 22;
+$c = 12;
+$d = $b - $c;
+
+echo "Belajar PHP Dasar 1 <br>";
+echo "Hello, Nama saya <strong>$a</strong>, saya berusia <strong>$b</strong> tahun. <br>";
+echo "Selisih umur saya dengan adek adalah = $d tahun.";
+
+ ?>
